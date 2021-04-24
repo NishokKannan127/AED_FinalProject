c319aec66449f37f886dde5a4b4f4a7e33d7efac
Kannan Nishok <nishok.k@northeastern.edu>
2021-04-23 22:30:27 -0400

    Remove errors

--- RapidExpress/src/Business/DB4OUtil/DB4OUtil.java
+++ RapidExpress/src/Business/DB4OUtil/DB4OUtil.java
@@ -74,8 +74,9 @@ public class DB4OUtil {
             system = ConfigureASystem.configure();  // If there's no System in the record, create a new one
         }
         else{
-            // system = systems.get(systems.size() - 1);
             system = ConfigureASystem.configure();
+            //system = systems.get(systems.size() - 1);
+ //           system = ConfigureASystem.configure();
         }
         conn.close();
         return system;
